2cb3142ff3303866253b70b9ea97e2f61b80a428
yasirshahid <66242488+yasirshahid@users.noreply.github.com>
2020-06-15 08:25:22 -0700

    Update App.js
    Buttons added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [count,setcount]=useState(0);
+
   return (
     <div className="App">
-      <h1>Test 1</h1>
+      <h1>This is a Test counter {count}</h1>
+      <button onClick={()=>setcount(++count)}> Value + </button>
+      <button onClick={()=>setcount(--count)}> Value - </button>
+      <br/>
+      <button onClick={()=>setcount(0)}> Update </button>
+
     </div>
   );
 }
