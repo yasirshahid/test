2e53b1490aafcf8fa4191349e4324fda7ede87df
yasirshahid <66242488+yasirshahid@users.noreply.github.com>
2020-06-15 08:58:04 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 import './App.css';
-import Message from './message';
+import Message from './message.js';
 
 function App() {
   let [count,setcount]=useState(0);
+  let [isMorning,setMorning]=useState(true);
 
   return (
-    <div className="App">
+    <div className="box">
+      
       <Message counter={count}/>
       <button onClick={()=>setcount(++count)}> Value + </button>
       <button onClick={()=>setcount(--count)}> Value - </button>
       <br/>
       <button onClick={()=>setcount(0)}> Update </button>
+      
 
     </div>
   );
